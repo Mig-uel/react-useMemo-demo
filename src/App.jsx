b433cd4c939fa3700324e362cc73ec6ec73b0bb6
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-12-21 17:36:10 -0500

    selecteditems memoized with useMemo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useState, useMemo, useRef } from 'react'
+import { useState, useMemo } from 'react'
 import { initialItems } from './utils'
 
 function App() {
   const [count, setCount] = useState(0)
   const [items] = useState(initialItems)
 
-  const selectedItem = items.find((item) => item.isSelected)
+  // const selectedItem = items.find((item) => item.isSelected)
+  const selectedItemsMemo = useMemo(
+    () => items.find((item) => item.isSelected),
+    [items]
+  )
   const handleClick = () => setCount(count + 1)
 
   return (
     <div>
       <h1>Count: {count}</h1>
-      <h1>Selected Item: {selectedItem.id}</h1>
+      <h1>Selected Item: {selectedItemsMemo.id}</h1>
 
       <button onClick={handleClick}>Increment</button>
     </div>
