0e111ed472b48939a3d2051748ec75c187beeee7
Miguel <35431824+Mig-uel@users.noreply.github.com>
2023-12-21 17:26:58 -0500

    initial code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { useState, useMemo, useRef } from 'react'
+import { initialItems } from './utils'
+
 function App() {
-  return <></>
+  const [count, setCount] = useState(0)
+  const [items] = useState(initialItems)
+
+  const selectedItem = items.find((item) => item.isSelected)
+  const handleClick = () => setCount(count + 1)
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <h1>Selected Item: {selectedItem.id}</h1>
+
+      <button onClick={handleClick}>Increment</button>
+    </div>
+  )
 }
 
 export default App
